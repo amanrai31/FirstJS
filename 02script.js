800e7edb0c3934eeb9976c2b471380c20efe2409
AMAN RAI <aman970rai@gmail.com>
2024-01-17 18:02:36 +0530

    first commit

--- 02script.js
+++ 02script.js
@@ -3,6 +3,5 @@ console.log("1" + 2);
 console.log(1+"2");
 console.log("1"+2+2);
 console.log(1+2+"2");              // here it adds
-
 console.log("2"==2);               // converts string 2 to number and then compares
 console.log("2"===2);               // check the type and throw false if not matched
