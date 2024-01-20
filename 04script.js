0d0cfc3d2100182b4b59d198153e6125e059b5d6
amanrai31 <aman970rai@gmail.com>
2024-01-20 21:07:53 +0530

    Numbers and Maths

--- 04script.js
+++ 04script.js
@@ -1,15 +1,16 @@
-let num1= new Number(100.654848466484);   // now we cant change its type by normal means
+                     //OPERATIONS, Numbers, Maths
+
+
+let num1= new Number(100.654848466484);   // now we cant change its type by normal means, see properties in console
 console.log(num1);
 
 console.log(num1.toString());
 
 console.log(num1.toPrecision(9));           // total pricision 9----- returns string
 
-console.log(num1);
-
 let num2= new Number(56);
 
-console.log(num2.toFixed(3));                   // digits after decimal
+console.log(num2.toFixed(2));           // digits after decimal, usually used upto 2 digit pricision
 
 let max= Number.MAX_VALUE;
 console.log(max);
@@ -21,3 +22,8 @@ console.log(Math.max(4,3,6,8));
 console.log(Math.min(4,3,6,8));
 
 console.log(Math.random());              // gives values between 0 and 1 >> play with it
+console.log(45%7);
+
+//Math.sqrt(); Math.cbrt(); Math.floor(); Math.ceil(); Math.abs();
+
+// Arithmatic operations>> +,-,*,/,%
